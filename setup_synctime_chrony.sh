964d682273f005238d109c2dee645fe929f106ab
Sawit M <peepeepopapapeepeepo@users.noreply.github.com>
2020-07-01 20:18:06 +0700

    Update setup_synctime_chrony.sh

--- setup_synctime_chrony.sh
+++ setup_synctime_chrony.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-if [ "${1}" == "TLS" -o "${1}" == "SILA" ]
-then
-    yum -y install chrony
-    cp /etc/chrony.conf{,.org}
-	echo -e 'server time.navy.mi.th iburst\nserver time2.navy.mi.th iburst' > /etc/chrony.conf
-    /bin/systemctl start chronyd
-    /bin/systemctl enable chronyd
-fi
+yum -y install chrony
+cp /etc/chrony.conf{,.org}
+echo -e 'server time.navy.mi.th iburst\nserver time2.navy.mi.th iburst' > /etc/chrony.conf
+/bin/systemctl start chronyd
+/bin/systemctl enable chronyd
